8514de247ebc625b9620d85ae2de9ba5421c2624
Tudor <xenogenesis@openmailbox.org>
2016-09-16 22:17:39 +0300

    fix manpage installing

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 PREFIX = /usr/local
-MANPREFIX = /usr/local/share
-MANDIR = $(MANPREFIX)/man/man1
+MANPREFIX = /usr/local/share/man
+MANDIR = $(MANPREFIX)/man1
 
 CFLAGS += -std=c99 -Wextra -g
 LDFLAGS += -lxcb -lxcb-ewmh -lxcb-icccm -lxcb-randr
