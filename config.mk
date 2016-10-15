c54351f1b8b7a44aefce87df67255a56b5d32ddf
Tudor Roman <xenogenesis@openmailbox.org>
2016-10-15 14:17:48 +0300

    remove debug flag

--- config.mk
+++ config.mk
@@ -2,5 +2,5 @@ PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 MANDIR = $(MANPREFIX)/man1
 
-CFLAGS += -std=c99 -Wextra -g -DD
+CFLAGS += -std=c99 -Wextra -g
 LDFLAGS += -lxcb -lxcb-ewmh -lxcb-icccm -lxcb-randr
